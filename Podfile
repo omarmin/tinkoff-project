1239e683edd5f28ba4d6a37035fa9194fcf49431
Omar Minkailov <>
2020-02-21 18:30:46 +0300

    Initial commit

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '13.0'
+
+target 'TinkoffProject' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for TinkoffProject
+  pod 'SwiftLint'
+  pod 'R.swift'
+
+end
---
+++ TinkoffProject/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  TinkoffProject
+//
+//  Created by Omar on 21.02.2020.
+//  Copyright © 2020 tinkoff-group-5. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
