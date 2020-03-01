713f1f776c365c9744bef71bd53fbe8a3630274e
Gennady Stepanov <gennady.stepanoff@flippingbook.com>
2020-03-01 17:49:03 +0300

    Preliminary implementation of DatabaseManager

---
+++ TinkoffProject/Services/Database Manager/CoredData Classes/Ticker+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Ticker+CoreDataClass.swift
+//  
+//
+//  Created by Gennady Stepanov on 29/02/2020.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Ticker)
+public class Ticker: NSManagedObject {
+
+}
---
+++ TinkoffProject/Services/Database Manager/Tickers/TickerData.swift
@@ -0,0 +1,15 @@
+//
+//  TickerData.swift
+//  TinkoffProject
+//
+//  Created by Gennady Stepanov on 01/03/2020.
+//  Copyright © 2020 tinkoff-group-5. All rights reserved.
+//
+
+import Foundation
+
+struct TickerData {
+  let description: String?
+  let symbol: String
+  let displaySymbol: String?
+}
